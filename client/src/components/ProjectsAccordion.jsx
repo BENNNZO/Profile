c9de7128149ce39bf5d2171143eef577e91e9fd8
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-30 17:53:48 -0400

    added link

--- client/src/components/ProjectsAccordion.jsx
+++ client/src/components/ProjectsAccordion.jsx
@@ -22,7 +22,7 @@ export default function ProjectsAccordion() {
             desc: '(Website not deployed yet) This is a website made for a property managment company with a working contact form and page that allows users to apply to rent a house digitally.',
             src: RiverOak,
             active: false,
-            link: "#not-deployed-anymore"
+            link: "https://river-oak-properties.herokuapp.com"
         },
         {
             title: 'Snippity',
